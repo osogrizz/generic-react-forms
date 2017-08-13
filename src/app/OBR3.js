a83b3eab69b9dec5ac90dbbdc01837fd2d2e27c5
Leo Torres <leo.htor@gmail.com>
2017-08-13 16:37:17 -0400

    added delete func to dynamically created input fields

--- src/app/OBR3.js
+++ src/app/OBR3.js
@@ -1,72 +1,64 @@
 import React from 'react'
-// import { Field, FieldArray, reduxForm } from 'redux-form'
-
-
-class InputField extends React.Component {
-    render() {
-      const { name, onChange } = this.props;
-      return (<div className="row">
-                <div className="input-field col s12">
-                  <label htmlFor={name}>OBR3</label>
-                  <input id={name}
-                         type="text"
-                         className=""
-                         value={this.props.inputValues}
-                         onChange={this.props.onChange} />
-
-                         <button type='delete' value='Delete' onClick={this.props.handleRemove}>
-                           <i className="material-icons">remove</i>
-                         </button>
-                </div>
-         </div>
-       )
-    }
-}
 
 export class OBR3 extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      inputValues: {},
-      inputs: []
+      value: [],
+      count: 1
     }
 
   }
 
-  handleRemove (event) {
-    let inputs = this.state.inputs
-    inputs.remove(event.target.inputs)
-    this.setState({ inputs })
+  handleChange (i, event) {
+    let value = this.state.value.slice()
+    value[i] = event.target.value
+    this.setState({ value })
   }
 
-  handleChange (name, event) {
-    let inputValues = this.state.inputValues
-    inputValues[name] = event.target.value
-    this.setState({ inputValues })
+
+  handleRemove (i) {
+    let value = this.state.value.slice()
+    value.splice(i, 1)
+    this.setState({
+      count: this.state.count - 1,
+      value
+    })
   }
 
-  handleAddInput() {
-      const name = `OBR3-${(this.state.inputs).length}`;
-
-      let inputBox = <InputField key={this.state.inputs.length}
-                                 name={name}
-                                 onChange={this.handleChange.bind(this, name)} />
-
-
-      const inputs = this.state.inputs
-      inputs.push( inputBox );
-      this.setState({ inputs });
+  handleAddInput () {
+    this.setState({count: this.state.count + 1})
   }
+
+
+  createInput() {
+      let inputItems = []
+      for (let i = 0; i < this.state.count; i++) {
+        inputItems.push(
+          <div key={i}>
+            <input className='OBR3' type='text' value={this.state.value[i]} onChange={this.handleChange.bind(this, i)} />
+            <button type='delete' value='Delete' onClick={this.handleRemove.bind(this, i)}>
+              <i className="material-icons">remove</i>
+            </button>
+          </div>
+        )
+      }
+      return inputItems || null
+    }
 
 
   render() {
     return (
       <div>
         <p id='OBR3'>OBR3</p>
-        <button type='submit' value='Submit' onClick={this.handleAddInput.bind(this)}><i className="material-icons">add</i></button>
-        {this.state.inputs}
+        <button type='submit' value='Submit' onClick={this.handleAddInput.bind(this)}>
+          <i className="material-icons">add</i>
+        </button>
+        {this.createInput()}
       </div>
     )
   }
 }
+
+{/* <RemoveInput onClick={this.handleRemove.bind(this)} /> */}
--- src/app/PID3.js
+++ src/app/PID3.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export class PID3 extends React.Component {
+
   render() {
     return (
       <div className="">
